e36208b1fd47955b069f9a204f714d4bf7067f1a
konchu974 <156214774+konchu974@users.noreply.github.com>
2024-02-29 14:22:05 +0100

    la on est sur la fin

--- page3.php
+++ page3.php
@@ -1,13 +1,4 @@
-<?php
-require_once("manipulation.php");
-
-session_start();
-
-?>
-
-
 <html lang="fr">
-
 <head>
     <title>Beat Connect</title>
     <link rel="stylesheet" href="pageco.css">
@@ -16,59 +7,48 @@ session_start();
     <script src="https://kit.fontawesome.com/dc5f9d95ad.js" crossorigin="anonymous"></script>
 </head>
 
-<header>
-    <img id="logo-sfond" src="image/Music-Logo-sans-fond.png" alt="Music-Logo-sans-fond" width="100" height="66.6">
-    <div class="dropdown">
-        <i class="fa-regular fa-user fa-2xl " style="color: #000000;"></i>
-        <div class="dropdown-content">
-            <a href="profil.php">Profil</a>
-            <a href="logout.php">Se deconnecter</a>
-        </div>
-    </div>
-
-    <h1>Beat Connect</h1>
-</header>
-
 <body>
-            <?php
-           echo "<div class='playlist-container'>";
+    <header>
+        <img id="logo-sfond" src="image/Music-Logo-sans-fond.png" alt="Music-Logo-sans-fond" width="100" height="66.6">
+        <div class ="connexion">
+            <a href="connection.html">
+                <i class="fa-regular fa-user fa-2xl " style="color: #2e2e2e;"></i>
+            </a>
+        </div>
+        <h1>Beat Connect</h1>
+    </header>
 
-           while ($row = $stmt->fetch()) {
-            if ($row['nom_playlist'] != $current_playlist) {
-                if ($current_playlist !== null) {
-                    echo "</div>"; // Fermer la précédente div de la playlist
-                }
-                echo "<div>"; // Ouvrir une nouvelle div pour la playlist
-                echo "<h2>Playlist : " . $row['nom_playlist'] . "</h2>";
-                echo "<img src='" . $row['playlist_photo'] . "' alt='Photo de la playlist' class='playlist-photo' onclick=\"toggleMusic('playlist_" . $row['Id_play'] . "')\">";
-                $current_playlist = $row['nom_playlist']; // Mettre à jour la playlist actuelle
+    <?php
+    require_once("manipulation.php");
+    echo "<div class='playlist-container'>";
+
+    while ($row = $stmt->fetch()) {
+        if ($row['nom_playlist'] != $current_playlist) {
+            if ($current_playlist !== null) {
+                echo "</div>"; // Fermer la div pour la playlist précédente
             }
-        
-            // Condition pour parcourir les musiques de la playlist
-            if ($row['nom_playlist'] == $current_playlist) {
-                echo "<div>";
-                echo "<h3>Musique : " . $row['nom_musique'] . "</h3>";
-                echo "<img src='" . $row['musique_photo'] . "' alt='Photo de la musique' class='musique-photo'>";
-                echo "</div>";
-            }
+            echo "<div class='playlist'>"; // Nouvelle div pour la playlist
+            echo "<h2>Playlist : " . $current_playlist . "</h2>";
+            echo "<button onclick=\"toggleMusic('playlist_" . $row['Id_play'] . "')\">Voir les musiques</button>";
+            echo "<img src='" . $row['playlist_photo'] . "' alt='Photo de la playlist' class='playlist-photo'>";
+            // Ajouter un identifiant unique à la liste de musiques de la playlist
+            echo "<ul id='playlist_" . $row['Id_play'] . "' style='display:none;'>";
+            $current_playlist = $row['nom_playlist']; // Mettre à jour la playlist actuelle
         }
-        
-        if ($current_playlist !== null) {
-            echo "</div>"; // Fermer la dernière div de la playlist
-        }
-        
-          ?>
-        </body>
 
-</html>
+        echo "<li>";
+        echo "Musique: " . $row['nom_musique'] . " - Durée: " . $row['duree_SC'];
+        echo "<br>";
+        echo "Photo: <img src='" . $row['musique_photo'] . "' alt='Photo de la musique' class='musique-photo'><br>";
+        echo "</li>";
+    }
 
-
-
-
-
-
-
-
-
+    if ($current_playlist !== null) {
+        echo "</ul>";
+        echo "</div>"; // Fermer la dernière div pour la dernière playlist
+    }
+    ?>
+     <script src="script.js"></script>
+</body>
 
 </html>
